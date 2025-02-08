2d82beadc7f4c089875496feb9845b1a45a656fc
Saaheer <saaheer.purav@gmail.com>
2025-02-08 16:03:16 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import Landing from './Landing';
-import Checkin from './Checkin';
 import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -10,7 +9,6 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Landing/>}/>
-          <Route path='/checkin' element={<Checkin/>}/>
         </Routes>
       </Router>
     </>
