e33c3d3be59ff1b21fb07b1ce62c5d355b7e7e22
Panchadip-128 <panchadip128@gmail.com>
2025-02-10 20:27:29 +0530

    Images Trial

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,7 +26,7 @@ function Footer() {
             <div className="footer-map text-left mb-4">
                 <h3 className="text-left mb-2">Find us at</h3>
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31083.29798917921!2d77.59631590000001!3d13.136376300000013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1739194998605!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31083.29798917921!2d77.59631590000001!3d13.136376300000013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1739194998605!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
                     width="100%"
                     height="200"
                     style={{ border: 0 }}
